7fd3e4cbf180eea3f662e1ea0977b0a381c86a45
Mwape <80482634+mwapevi@users.noreply.github.com>
2026-06-06 00:17:01 +0200

    webhook.py

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -1,1 +1,14 @@
+from fastapi import APIRouter
 
+router = APIRouter()
+
+
+@router.post("/webhook/events")
+async def receive_event(
+    payload: dict
+):
+
+    return {
+        "message": "event received",
+        "event_id": payload["event_id"]
+    }
