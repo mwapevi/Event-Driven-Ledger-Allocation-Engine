980e591b48ee72b88bd3c38a523c587403a931b9
Victor Mwape <80482634+mwapevi@users.noreply.github.com>
2026-06-15 21:38:03 +0200

    session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:simple123@localhost:5432/ledger_allocation_engine"
+DATABASE_URL = "postgresql://postgres:yourpassword@localhost:5432/ledger_allocation_engine"
 
 engine = create_engine(DATABASE_URL)
 
