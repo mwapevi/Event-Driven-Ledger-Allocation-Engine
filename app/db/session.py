73c88babc3b9caebe6839b8f48830a5711199812
Victor Mwape <mwape.victor@gmail.com>
2026-06-17 20:20:57 +0200

    security: externalize database credentials

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:simple123@localhost:5432/ledger_allocation_engine"
+DATABASE_URL = "postgresql://postgres:yourownpassword@localhost:5432/ledger_allocation_engine"
 
 #DATABASE_URL = os.getenv(
     #"DATABASE_URL",
